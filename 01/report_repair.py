d3691fadb218445af0c0aba821a4d425d659a0a0
Tim Leonhard <t_leonhard18@cs.uni-kl.de>
2020-12-01 13:59:17 +0100

    day01: change paramter name

--- 01/report_repair.py
+++ 01/report_repair.py
@@ -1,15 +1,15 @@
 import itertools
 
-def getSummands(l: list, result: int, tuple_size: int) -> tuple:
-	for tup in itertools.combinations(l, tuple_size):
+def getSummands(l: list, result: int, n_summands: int) -> tuple:
+	for tup in itertools.combinations(l, n_summands):
 	 	if sum(tup) == result:
 	 		return tup
 
 with open("input.txt") as input_file:
 	numbers = [int(line) for line in input_file]
 
-a, b = getSummands(numbers, result=2020, tuple_size=2)
+a, b = getSummands(numbers, result=2020, n_summands=2)
 print(f"Part 1: {a*b}")
 
-a, b, c = getSummands(numbers, result=2020, tuple_size=3)
+a, b, c = getSummands(numbers, result=2020, n_summands=3)
 print(f"Part 2: {a*b*c}")
