1b24312a1b7ace7d92a1af4913ce144ddedf28f3
Tim Leonhard <t_leonhard18@cs.uni-kl.de>
2020-12-10 09:27:01 +0100

    day10: switch to recursive approach

--- 10/adapter_array.py
+++ 10/adapter_array.py
@@ -1,40 +1,21 @@
 import time
-import itertools
 
 begin = time.time()
 
 ###
 
-# Found through testing:
-# k = length of a sequence of 1-jolt differences
-# v = number of valid arrangements
-arrangements_per_one_seq = {
-	0: 1,
-	1: 1,
-	2: 2,
-	3: 4,
-	4: 7,
-	5: 13,
-	6: 24,
-	7: 44,
-	8: 81,
-	9: 149,
-	10: 274,
-	11: 504,
-	12: 927,
-	13: 1705,
-	14: 3136,
-	15: 5768,
-	16: 10609,
-	17: 19513,
-	18: 35890
-}
+def arrangements(seq_len: int) -> int:
+	if seq_len < 0:
+		return 0
+	if seq_len == 0:
+		return 1
+	return arrangements(seq_len-1) + arrangements(seq_len-2) + arrangements(seq_len-3)
 
 
-def getArrangements(seqs: list, d: dict) -> int:
+def product(factors: list) -> int:
 	res = 1
-	for s in seqs:
-		res *= d[len(s)]
+	for f in factors:
+		res *= f
 	return res
 
 
@@ -45,9 +26,10 @@ j_out = [0] + adapters + [adapters[-1] +3]
 diffs = [0] + [j - j_out[idx] for idx, j in enumerate(j_out[1:])]
 
 one_seqences = "".join(str(n) for n in diffs[1:]).split("3")
+arrangements_per_seq = [arrangements(len(seq)) for seq in one_seqences]
 
-print(f"Part 1: {diffs.count(1)*diffs.count(3)}")
-print(f"Part 2: {getArrangements(one_seqences, arrangements_per_one_seq)}")
+print(f"Part 1: {diffs.count(1) * diffs.count(3)}")
+print(f"Part 2: {product(arrangements_per_seq)}")
 
 ###
 
