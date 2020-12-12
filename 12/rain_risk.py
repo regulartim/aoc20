268a165305a13343724fdaebd01fa34de4a3182b
Tim Leonhard <t_leonhard18@cs.uni-kl.de>
2020-12-12 10:50:06 +0100

    day12: refactor and clean up

--- 12/rain_risk.py
+++ 12/rain_risk.py
@@ -4,8 +4,7 @@ begin = time.time()
 
 ###
 
-DIRECTIONS = [(1,0),(0,-1),(-1,0),(0,1)]
-CELESTIAL = {
+DIRECTIONS = {
 	"E": (1,0),
 	"S": (0,-1),
 	"W": (-1,0),
@@ -13,65 +12,72 @@ CELESTIAL = {
 }
 
 
-def move(current: dict, instr: tuple) -> dict:
+def move(point: tuple, di: int, instr: tuple) -> tuple:
 	action, v = instr
 
 	if action in "NSWEF":
-		x, y = DIRECTIONS[current["dir"]] if action == "F" else CELESTIAL[action]
-		curr_x, curr_y = current["pos"]
-		current["pos"] = (curr_x+x*v, curr_y+y*v)
+		x, y = DIRECTIONS[action] if action != "F" else list(DIRECTIONS.values())[di]
+		curr_x, curr_y = point
+		point = (curr_x+x*v, curr_y+y*v)
 
 	else:
 		idx_change = v//90
-		new_dir = current["dir"] + (idx_change if action == "R" else -idx_change)
-		current["dir"] = new_dir%4
+		new_dir = di + (idx_change if action == "R" else -idx_change)
+		di = new_dir % 4
 
-	return current
+	return point, di
 
 
-def rotate(wp: tuple, instr: tuple) -> tuple:
-	x,y = wp
+def rotate(point: tuple, instr: tuple) -> tuple:
+
+	x, y = point
 	action, v = instr
 	steps = v//90
+
 	for _ in range(steps):
 		if action == "L":
 			x, y = -y, x
 		if action == "R":
 			x, y = y, -x
-	return (x,y)
 
-def manhattan(point: tuple) -> int:
+	return x, y
+
+
+def moveWaypoint(point: tuple, instr) -> tuple:
+	action, _ = instr
+	if action in "LR":
+		return rotate(point, instr)
+	return move(point, 0, instr)[0]
+
+
+def movePartTwo(point: tuple, wp: tuple, instr: tuple) -> tuple:
+	action, v = instr
+	if action == "F":
+		point = (point[0] + wp[0]*v), (point[1] + wp[1]*v)
+	else:
+		wp = moveWaypoint(wp, instr)
+
+	return point, wp
+
+
+def manhattanDistance(point: tuple) -> int:
 	return sum([abs(v) for v in point])
+
 
 with open("input.txt") as input_file:
 	instructions = [(line[0], int(line[1:])) for line in input_file]
 
-#print(instructions)
-
-status = {
-	"pos": (0,0),
-	"dir": 0
-}
-
-for i in instructions:
-	status = move(status, i)
-
-print(f"Part 1: {manhattan(status['pos'])}")
-
+p1_position, p2_position = (0,0), (0,0)
+direction = 0
 waypoint = (10,1)
-position = (0,0)
 
 for i in instructions:
-	if i[0] == "F":
-		movement = (waypoint[0]*i[1], waypoint[1]*i[1])
-		position = (position[0]+movement[0], position[1]+movement[1])
-	elif i[0] in "LR":
-		waypoint = rotate(waypoint, i)
-	else:
-		tmp = {"pos": waypoint, "dir": 0}
-		waypoint = move(tmp, i)["pos"]
+	p1_position, direction = move(p1_position, direction, i)
+	p2_position, waypoint = movePartTwo(p2_position, waypoint, i)
 
-print(f"Part 2: {manhattan(position)}")
+
+print(f"Part 1: {manhattanDistance(p1_position)}")
+print(f"Part 2: {manhattanDistance(p2_position)}")
 
 ###
 
