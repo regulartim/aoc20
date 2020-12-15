cc963d20f6c06a4a19bb7220ed767f430f28df89
Tim Leonhard <t_leonhard18@cs.uni-kl.de>
2020-12-15 08:29:06 +0100

    day15: optimization by changing dict values from list to int

--- 15/rambunctious_recitation.py
+++ 15/rambunctious_recitation.py
@@ -11,19 +11,18 @@ starting_numbers = [int(n) for n in RAW_INPUT.split(",")]
 def calculateNthNumber(n: int, numbers: list) -> int:
 	d = dict()
 	for idx, num in enumerate(numbers):
-		d[num] = [idx]
+		d[num] = idx
 
 	number = numbers[-1]
 	for turn in range(len(numbers), n):
 
-		spoken_in_turns = d[number]
+		has_been_spoken = number in d
+		last_spoken = d.get(number, 0)
+		
+		d[number] = turn-1
+		number = (turn-1) - last_spoken	if has_been_spoken else 0
 
-		if len(spoken_in_turns) < 2:
-			number = 0
-		else:
-			number = spoken_in_turns[-1] - spoken_in_turns[-2]
-
-		d.setdefault(number,[]).append(turn)
+			
 
 	return number
 
