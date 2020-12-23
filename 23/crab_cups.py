6af79f817792a108f043aed26b65a4fea7dd7bbb
Tim Leonhard <t_leonhard18@cs.uni-kl.de>
2020-12-23 21:10:26 +0100

    day23: implement pickup handling inside data structure to improve performance

--- 23/crab_cups.py
+++ 23/crab_cups.py
@@ -21,6 +21,7 @@ class circularlist:
 		self.lookup = dict()
 		self.current = None
 		self.last = None
+		self.pickup = None
 
 		for value in l:
 			if self.current is None:
@@ -32,10 +33,17 @@ class circularlist:
 			else:
 				self.insert(value, self.last.value)
 
-	def rotate(self, steps: int):
-		for _ in range(steps):
-			self.current = self.current.next_node
-			self.last = self.last.next_node
+	def insert(self, value, predecessor):
+		n = node(value)
+		pred = self.lookup[predecessor]
+		n.next_node = pred.next_node
+		pred.next_node = n
+		if pred == self.last:
+			self.last = n
+		self.lookup[value] = n
+
+	def append(self, value):
+		self.insert(value, self.last.value)
 
 	def popleft(self):
 		res = self.current.value
@@ -48,23 +56,53 @@ class circularlist:
 		self.lookup.pop(res)
 		return res
 
-	def insert(self, value, behind):
-		n = node(value)
-		pred = self.lookup[behind]
-		n.next_node = pred.next_node
-		pred.next_node = n
+	def rotate(self, steps: int):
+		for _ in range(steps):
+			self.current = self.current.next_node
+			self.last = self.last.next_node
+
+	def pushToPickup(self, predecessor, n=3):
+		pred = self.lookup[predecessor]
+		self.pickup = pred.next_node
+		for _ in range(n):
+			pred.next_node = pred.next_node.next_node
+
+	def insertPickup(self, predecessor, n=3):
+		pred = self.lookup[predecessor]
+		last_pickup = self.pickup
+		for _ in range(n-1):
+			last_pickup = last_pickup.next_node
+		last_pickup.next_node = pred.next_node
+		pred.next_node = self.pickup
 		if pred == self.last:
-			self.last = n
-		self.lookup[value] = n
+			self.last = last_pickup
+		self.pickup = None
 
-	def isEmpty(self):
-		return self.current is None
+	def getPickup(self, n=3) -> set:
+		res = set()
+		if self.pickup is None:
+			return res
+		elem = self.pickup
+		for _ in range(n):
+			res.add(elem.value)
+			elem = elem.next_node
+		return res
 
-	def append(self, value):
-		self.insert(value, self.last.value)
+	def getList(self) -> list:
+		res = list()
+		n = self.current
+		res.append(n.value)
+		while n != self.last:
+			n = n.next_node
+			res.append(n.value)
+		return res
 
 	def maxValue(self) -> int:
 		return max(self.lookup.keys())
+
+	def isEmpty(self) -> bool:
+		return self.current is None
+
 
 
 def play(cups: circularlist, moves: int) -> list:
@@ -74,16 +112,16 @@ def play(cups: circularlist, moves: int) -> list:
 	for _ in range(moves):
 
 		current = cups.current.value
+		cups.pushToPickup(predecessor=current)
 		cups.rotate(1)
 
-		pick_up = [cups.popleft() for _ in range(3)]
+		pick_up = cups.getPickup()
 
 		destination = current -1 if current > 1 else highest_value
 		while destination in pick_up:
 			destination = destination -1 if destination > 1 else highest_value
 
-		for p in reversed(pick_up):
-			cups.insert(p, behind=destination)
+		cups.insertPickup(predecessor=destination)
 
 	return cups
 
@@ -107,12 +145,13 @@ def getP2Result(q: circularlist) -> int:
 
 
 cup_list_p1 = circularlist([int(n) for n in RAW_INPUT])
+cup_list_p1 = play(cup_list_p1, moves=100)
+
 cup_list_p2 = circularlist([int(n) for n in RAW_INPUT])
 
 for i in range(9, 1000000):
 	cup_list_p2.append(i+1)
 
-cup_list_p1 = play(cup_list_p1, moves=100)
 cup_list_p2 = play(cup_list_p2, moves=10000000)
 
 print(f"Part 1: {getResultString(cup_list_p1)}")
